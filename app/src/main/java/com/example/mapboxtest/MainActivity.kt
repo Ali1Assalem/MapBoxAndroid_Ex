5dd088632a75fdd19a3ee0e01b707ada8be6e126
ALI <ali11salem00@gmail.com>
2022-10-13 10:29:14 +0300

    Update Dependency Versions

--- app/src/main/java/com/example/mapboxtest/MainActivity.kt
+++ app/src/main/java/com/example/mapboxtest/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.mapboxtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.mapboxtest.databinding.ActivityMainBinding
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Mapbox.getInstance(applicationContext , getString(R.string.mapbox_access_token))
+        binding.mapView.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        if (outState != null){
+            binding.mapView.onSaveInstanceState(outState)
+        }
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
